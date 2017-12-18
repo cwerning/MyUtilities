279db56b922de09101d28fe0fcb8b2acca5ee1e7
L115student <L115student@L115-08-043627.bitnet.wctc.edu>
2017-12-18 15:32:02 -0600

    Finishing up Date Util for final

--- src/edu/wctc/advjava/jgl/datetime/DateUtilities.java
+++ src/edu/wctc/advjava/jgl/datetime/DateUtilities.java
@@ -12,23 +12,25 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 
-
-
 /**
- * This class is for handling dates. It has methods for converting LocalDateTime objects
- * to Strings and methods for converting Strings to LocalDate objects.
+ * This class is for handling dates. It has methods for converting LocalDateTime
+ * objects to Strings and methods for converting Strings to LocalDate objects.
+ *
  * @author Chad Werning, cwerning@my.wctc.edu
  * @version 1.00
  * @since 1.8
  */
 public class DateUtilities {
-/**
- * This method takes a localDateTime objects and turns it into a string that is formatted
- * as a BASIC_ISO_DATE. 
- * @param date
- * @return String of the LocalDateTime object that was passed in the parameters.
- */
-    public String toString(LocalDateTime date) {
+
+    /**
+     * This method takes a localDateTime objects and turns it into a string that
+     * is formatted as a BASIC_ISO_DATE.
+     *
+     * @param date
+     * @return String of the LocalDateTime object that was passed in the
+     * parameters.
+     */
+    public String toString(LocalDateTime date) throws IllegalArgumentException {
         if (date == null) {
             throw new IllegalArgumentException("Date cannot be null");
         }
@@ -36,16 +38,20 @@ public class DateUtilities {
         String stringDate = date.format(formatIt);
         return stringDate;
     }
-/**
- * This method takes LocalDateTime objects and a String of the pattern that you would like it
- * to be formatted to.
- * @param date
- * @param pattern
- * @exception IllegalArgumentException is thrown if pattern is null or empty.
- * @exception IllegalArgumentException is thrown if data is null.
- * @return String of the date object passed and formatted as the pattern the was passed.
- */
-    public String toString(LocalDateTime date, String pattern) {
+
+    /**
+     * This method takes LocalDateTime objects and a String of the pattern that
+     * you would like it to be formatted to.
+     *
+     * @param date
+     * @param pattern
+     * @exception IllegalArgumentException is thrown if pattern is null or
+     * empty.
+     * @exception IllegalArgumentException is thrown if data is null.
+     * @return String of the date object passed and formatted as the pattern the
+     * was passed.
+     */
+    public String toString(LocalDateTime date, String pattern) throws IllegalArgumentException {
         if (pattern == null || pattern.isEmpty()) {
             throw new IllegalArgumentException("Pattern cannot be null or empty.");
         } else if (date == null) {
@@ -56,61 +62,61 @@ public class DateUtilities {
         String stringDate = date.format(formatIt);
         return stringDate;
     }
-/**
- * This method takes a date string and formats it to the pattern of yyyy-MM-dd.
- * @param dateString
- * @return LocalDate of the date string that was passed. Formatted as yyyy=MM-dd.
- */
-    public LocalDate toDate(String dateString) {
+
+    /**
+     * This method takes a date string and formats it to the pattern of
+     * yyyy-MM-dd.
+     *
+     * @param dateString
+     * @return LocalDate of the date string that was passed. Formatted as
+     * yyyy=MM-dd.
+     */
+    public LocalDateTime toDate(String dateString) throws IllegalArgumentException {
         if (dateString == null || dateString.isEmpty()) {
             throw new IllegalArgumentException("Date String cannot be null or empty");
         }
 
-        DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate ldt = LocalDate.parse(dateString, dateformatter);
+        DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        LocalDateTime ldt = LocalDateTime.parse(dateString, dateformatter);
         return ldt;
     }
-/**
- * This method takes a date String and a pattern String and turns it into a LocalDate object.
- * @param dateString
- * @param pattern
- * @return A LocalDate object of the date String passed, formatted to the pattern String which was also passed.
- */
-    public LocalDate toDate(String dateString, String pattern) {
+
+    /**
+     * This method takes a date String and a pattern String and turns it into a
+     * LocalDateTime object.
+     *
+     * @param dateString
+     * @param pattern
+     * @return A LocalDateTime object of the date String passed, formatted to
+     * the pattern String which was also passed.
+     */
+    public LocalDateTime toDate(String dateString, String pattern) throws IllegalArgumentException {
         if (dateString == null || dateString.isEmpty()) {
             throw new IllegalArgumentException("Date String cannot be null or empty");
         } else if (pattern == null || pattern.isEmpty()) {
             throw new IllegalArgumentException("Pattern cannot be null or empty");
         }
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(pattern);
-        LocalDate ldt = LocalDate.parse(dateString, dateFormatter);
+        LocalDateTime ldt = LocalDateTime.parse(dateString, dateFormatter);
         return ldt;
     }
-/**
- * This method takes two LocalDateTime objects and returns the minutes between the two dates as data type long.
- * @param localDateTime1
- * @param localDateTime2
- * @return Calculated the minutes between two dates. Data type long.
- */
-    public long getDuration(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+
+    /**
+     * This method takes two LocalDateTime objects and returns the minutes
+     * between the two dates as data type long.
+     *
+     * @param localDateTime1
+     * @param localDateTime2
+     * @return Calculated the minutes between two dates. Data type long.
+     */
+    public double getDuration(LocalDateTime localDateTime1, LocalDateTime localDateTime2) throws IllegalArgumentException {
         if (localDateTime1 == null || localDateTime2 == null) {
             throw new IllegalArgumentException("Local Date Time objects cannot be null.");
         }
         Duration duration = Duration.between(localDateTime1, localDateTime2);
-        long minutes = Math.abs(duration.toMinutes()); 
+        double minutes = Math.abs(duration.toMinutes());
         return minutes;
 
-    }
-    
-    public static void main(String[] args) {
-        DateUtilities du = new DateUtilities();
-        
-        LocalDateTime toDateTime = LocalDateTime.of(2017, 12, 15, 9, 46, 45);
-        LocalDateTime fromDateTime = LocalDateTime.now();
-        
-        System.out.println(du.getDuration(fromDateTime, toDateTime));
-        
-        
     }
 
 }
