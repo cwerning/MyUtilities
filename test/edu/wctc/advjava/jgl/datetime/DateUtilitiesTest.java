526b3137d5fc34e654748bd73ac0159cf4f53016
L115student <L115student@L115-08-043627.bitnet.wctc.edu>
2017-12-18 16:13:05 -0600

    Finishing up Date Util for final

--- test/edu/wctc/advjava/jgl/datetime/DateUtilitiesTest.java
+++ test/edu/wctc/advjava/jgl/datetime/DateUtilitiesTest.java
@@ -7,27 +7,68 @@ package edu.wctc.advjava.jgl.datetime;
 
 import java.time.LocalDateTime;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
  * @author L115student
  */
 public class DateUtilitiesTest {
-    
+
+    private DateUtilities instance;
+
     public DateUtilitiesTest() {
     }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
     }
 
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        // Insead of initializing in each test, just do it here
+        // Declaration is above.
+        instance = new DateUtilities();
+    }
+
+    @After
+    public void tearDown() {
+        // clean up after yourself!
+        instance = null;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void toStringDateNullShouldThrowException() {
+        DateUtilities du = new DateUtilities();
+        du.toString(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void toStringWithPatterDateNullShouldThrowException() {
+        DateUtilities du = new DateUtilities();
+        du.toString(null, "yyyy/MM/dd");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void toStringWithPatternPatternNullShouldThrowException() {
+        DateUtilities du = new DateUtilities();
+        LocalDateTime ldt = LocalDateTime.now();
+        du.toString(ldt, null);
+    }
     
-    
+    @Test(expected = IllegalArgumentException.class)
+    public void toStringWithPatternPatternEmptyShouldThrowException() {
+        DateUtilities du = new DateUtilities();
+        LocalDateTime ldt = LocalDateTime.now();
+        du.toString(ldt, "");
+    }
+
 }
